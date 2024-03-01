351d9ba70af112824dd8f779c4412520cdda5f63
Pontuagi <benepontuagi@gmail.com>
2024-03-01 10:29:06 +0300

    profile pic correction

--- registration/models.py
+++ registration/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     # Profile Model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='media/profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
